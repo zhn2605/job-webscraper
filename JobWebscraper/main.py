d685b86867fdadb1523820ef2c7c8e046c8d515e
zhn <zzihan0805@gmail.com>
2024-10-17 18:18:34 -0400

    Fix OperaGX Webdriver 1

--- JobWebscraper/main.py
+++ JobWebscraper/main.py
@@ -3,35 +3,14 @@ from bs4 import BeautifulSoup
 import streamlit as st
 import pandas as pd
 
+from scrape import scrape_website
+
 st.title("Job Web Scraper")
 url = st.text_input("Enter a website URL: ")
 
-# url = "h ttps://en.wikipedia.org/wiki/List_of_Greek_mathematicians"
+# url = "https://en.wikipedia.org/wiki/List_of_Greek_mathematicians"
 
 if st.button("Scrape Site"):
     st.write("Scraping the website...")
-
-# Set up url, paege, soup
-
-page = requests.get(url)
-
-soup = BeautifulSoup(page.text, "html.parser")
-
-# List all names
-all_col_divs = soup.findAll("div", class_="div-col")
-
-# print(all_col_divs)
-
-name_section = all_col_divs[0]
-
-if name_section:
-    links = name_section.find_all("a")
-
-    # print(links)
-    # print("\n")
-
-    for name in links:
-        if "title" in name.attrs:
-            st.write(name.get("title"))
-else:
-    st.write("No such section.")
+    result = scrape_website(url)
+    print(result)
--- JobWebscraper/scrape.py
+++ JobWebscraper/scrape.py
@@ -5,14 +5,14 @@ import time
 def scrape_website(website):
     print("Launching browser...")
 
-    opera_driver_path = r"C:\ProgramData\Microsoft Visual Studio\operadriver_win64"
+    opera_driver_path = r"C:\Users\zziha\Projects\JobWebscraper\JobWebscraper\operadriver_win64\operadriver.exe"
     opera_exe_path = r"C:\Users\zziha\AppData\Local\Programs\Opera GX\opera.exe"
     
     options = webdriver.ChromeOptions()
     options.binary_location = opera_exe_path
     options.add_experimental_option('w3c', True)
     webdriver_service = Service(opera_driver_path)
-    driver = webdriver.Remote(service=(webdrivergit _service), options=options)
+    driver = webdriver.Remote(webdriver_service, options=options)
 
     try:
         driver.get(website)
@@ -23,5 +23,3 @@ def scrape_website(website):
         return html
     finally:
         driver.quit()
-
-
