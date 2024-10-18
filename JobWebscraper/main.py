26637b6e9449c7d5bcc172b3a80ff971969af261
zhn <zzihan0805@gmail.com>
2024-10-18 02:53:43 -0400

    Set up button & content for parsing in main.py

--- JobWebscraper/main.py
+++ JobWebscraper/main.py
@@ -10,3 +10,27 @@ st.title("Job Web Scraper")
 url = st.text_input("Enter a website URL: ")
 
 # url = "https://en.wikipedia.org/wiki/List_of_Greek_mathematicians"
+
+if st.button("Scrape Site"):
+    st.write("Scraping the website...")
+    result = scrape_website(url)
+    
+    result = scrape_website(url)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+
+    # Expanding to view more content
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleaned_content, height = 300)
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("What do you wish to parse?")
+
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing the content")
+
+            # Grab chunks to pass into llm
+            dom_chunks = split_dom_content(st.session_state.dom_content)
--- JobWebscraper/scrape.py
+++ JobWebscraper/scrape.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
 import time
@@ -5,7 +6,7 @@ import time
 def scrape_website(website):
     print("Launching browser...")
 
-    chrome_driver_path = "./chromedriver-win64\chromedriver.exe"
+    chrome_driver_path = r"C:\Users\zziha\Projects\JobWebscraper\JobWebscraper\chromedriver-win64\chromedriver.exe"
     # opera_exe_path = r"C:\Users\zziha\AppData\Local\Programs\Opera GX\opera.exe"
     
     options = webdriver.ChromeOptions()
@@ -15,14 +16,41 @@ def scrape_website(website):
 
     try:
         driver.get(website)
-        # Potential 
+        # Potential Captcha solver
 
 
         print("Page loaded...")
         html = driver.page_source
         time.sleep(10)
 
-
         return html
     finally:
         driver.quit()
+
+# Used to extract main content from html in a big str result
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.body
+
+    if body_content:
+        return str(body_content)
+    return ""
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, "html.parser")
+    
+    # Extract all the text that is not script or style
+    for script_or_style in soup(["script"], ["style"]):
+        script_or_style.extract()
+
+    cleaned_content = soup.get_text(separator="\n")     # Separate each line of text with a new line
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip()   # Remove any \n when unecessary (empty text strings)
+    )
+
+    return cleaned_content
+
+def split_dom_content(dom_content, max_length=6000):
+    return [
+        dom_content[i: i + max_length] for i in range(0, len(dom_content), max_length)     # Allow llm to process content in batches (of size 6000 lines)
+    ]
