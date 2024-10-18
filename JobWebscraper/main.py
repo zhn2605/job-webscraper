13f772002af630fc7c2c4734d4e8da471a4d0c0f
zhn <zzihan0805@gmail.com>
2024-10-18 02:42:59 -0400

    Created html functions for content handling

--- JobWebscraper/main.py
+++ JobWebscraper/main.py
@@ -1,16 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
 import streamlit as st
-import pandas as pd
-
-from scrape import scrape_website
+from scrape import (
+    scrape_website,
+    split_dom_content,
+    clean_body_content,
+    extract_body_content,
+)
 
 st.title("Job Web Scraper")
 url = st.text_input("Enter a website URL: ")
 
 # url = "https://en.wikipedia.org/wiki/List_of_Greek_mathematicians"
-
-if st.button("Scrape Site"):
-    st.write("Scraping the website...")
-    result = scrape_website(url)
-    print(result)
--- JobWebscraper/scrape.py
+++ JobWebscraper/scrape.py
@@ -5,24 +5,23 @@ import time
 def scrape_website(website):
     print("Launching browser...")
 
-    chrome_driver_path = "./chromedriver-win64/chromedriver.exe"
+    chrome_driver_path = "./chromedriver-win64\chromedriver.exe"
     # opera_exe_path = r"C:\Users\zziha\AppData\Local\Programs\Opera GX\opera.exe"
     
     options = webdriver.ChromeOptions()
-    # options.binary_location = opera_exe_path
-    # options.add_experimental_option('w3c', True)
-
-    # Create driver path
-    # webdriver_service = service.Service(opera_driver_path)
 
     # Init driver using chrome service and options
     driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
 
     try:
         driver.get(website)
+        # Potential 
+
+
         print("Page loaded...")
         html = driver.page_source
         time.sleep(10)
+
 
         return html
     finally:
